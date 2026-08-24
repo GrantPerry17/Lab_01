749deb69e96aeaf3eb7332bf66c3b0313adf643c
GrantPerry17 <perrygrant46@gmail.com>
2026-08-24 16:10:30 -0400

    Part 1 - Person

---
+++ src/PersonGenerator.java
@@ -0,0 +1,51 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PersonGenerator
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        ArrayList<String> persons = new ArrayList<>();
+
+        boolean morePersons;
+
+        do
+        {
+            String id = SafeInput.getNonZeroLenString(in, "Enter ID");
+            String firstName = SafeInput.getNonZeroLenString(in, "Enter First Name");
+            String lastName = SafeInput.getNonZeroLenString(in, "Enter Last Name");
+            String title = SafeInput.getNonZeroLenString(in, "Enter Title");
+            int yearOfBirth = SafeInput.getInt(in, "Enter Year of Birth");
+
+            String personRecord = id + ", " + firstName + ", " + lastName + ", " + title + ", " + yearOfBirth;
+
+            persons.add(personRecord);
+
+            morePersons = SafeInput.getYNConfirm(in, "Enter another person?");
+        } while(morePersons);
+
+        String fileName = SafeInput.getNonZeroLenString(in, "Enter file name");
+
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+
+            for(String person : persons)
+            {
+                writer.write(person);
+                writer.newLine();
+            }
+
+            writer.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error writing file: " + e.getMessage());
+        }
+    }
+}
